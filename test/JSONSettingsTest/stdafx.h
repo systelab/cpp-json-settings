051e82a8fe55d0923fc6d42c469ddb5e1e5588b9
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-27 21:07:47 +0100

    Added shared includes for unit tests

---
+++ test/JSONSettingsTest/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING 1
+
+// STL
+#include <memory>
+#include <string>
+
+// GoogleTest
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
