c7846ceac154920322ea9df34270aa21148ac321
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-30 00:56:07 +0100

    Created cache to handle settings values in memory

---
+++ src/JSONSettings/Model/SettingsCache.cpp
@@ -0,0 +1,58 @@
+#include "stdafx.h"
+#include "SettingCache.h"
+
+#include <boost/lexical_cast.hpp>
+
+
+namespace systelab { namespace setting {
+
+	SettingsCache::SettingsCache() = default;
+	SettingsCache::~SettingsCache() = default;
+
+	SettingsCache& SettingsCache::get()
+	{
+		static SettingsCache instance;
+		return instance;
+	}
+
+	template<typename Type>
+	bool SettingsCache::hasSetting(const SettingsFile& file,
+								   const SettingPath& sectionPath) const
+	{
+		if (m_cache.find(file) != m_cache.end())
+		{
+			const auto& fileSettings = m_cache.at(file);
+			return fileSettings.find(sectionPath) != fileSettings.end();
+		}
+
+		return false;
+	}
+
+	template <typename Type>
+	Type SettingsCache::getSetting(const SettingsFile& file,
+								   const SettingPath& sectionPath) const
+	{
+		auto itFile = m_cache.find(file);
+		if (itFile != m_cache.end())
+		{
+			auto itSetting = itFile->second.find(sectionPath);
+			if (itSetting != itFile->second.end())
+			{
+				return boost::lexical_cast<Type>(itSetting->second);
+			}
+		}
+
+		throw std::runtime_error("Setting '" + settingPath + "' of file '" + file + "' not in cache");
+	}
+
+	template <typename Type>
+	void SettingsCache::setSetting(const SettingsFile& file,
+								   const SettingPath& sectionPath,
+								   const Type& value)
+	{
+		std::stringstream oss;
+		oss << value;
+		m_cache[file][sectionPath] = oss.str();
+	}
+
+}}
---
+++ src/JSONSettings/Model/SettingsCache.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "SettingValue.h"
+
+
+namespace systelab { namespace setting {
+
+	class SettingsCache
+	{
+	private:
+		typedef std::string SettingsFile;
+		typedef std::string SettingPath;
+		typedef std::map< SettingsFile, std::map<SettingPath, std::string> > Container;
+
+	public:
+		SettingsCache();
+		~SettingsCache();
+
+		static SettingsCache& get();
+
+		template<typename Type>
+		bool hasSetting(const SettingsFile&, const SettingPath&) const;
+
+		template<typename Type>
+		Type getSetting(const SettingsFile&, const SettingPath&) const;
+
+		template<typename Type>
+		void setSetting(const SettingsFile&, const SettingPath&, const Type&);
+
+	private:
+		Container m_cache;
+	};
+
+}}
