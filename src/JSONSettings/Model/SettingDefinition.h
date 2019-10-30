7bf5f65a67c51063ea43bf5bfd9558d9b8faf914
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-30 01:26:37 +0100

    Improved cache member

--- src/JSONSettings/Model/SettingDefinition.h
+++ src/JSONSettings/Model/SettingDefinition.h
@@ -7,9 +7,9 @@ namespace systelab { namespace setting {
 
 	struct SettingDefinition
 	{
-		bool cache;
 		SettingValue defaultValue;
 		std::function<bool(SettingValue)> validationFunction;
+		bool useCache;
 	};
 
 }}
