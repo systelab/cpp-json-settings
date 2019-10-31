ace139cf859db5024fad4c8c7a9c4498712286fb
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-31 20:57:29 +0100

    Added test package for JSONSettings library

---
+++ test_package/JSONSettingsExample.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "JSONSettings/SettingsService.h"
+#include "JSONSettings/SettingDefinitionMacros.h"
+#include "JSONSettings/SettingsMacros.h"
+
+
+JSON_SETTINGS_FILE(TestSettingsFile, "PackageSettingsFile.json",
+	JSON_SETTING_INT(TestSetting, "test.setting.path", 789, false)
+)
+
+int main(int argc, char *argv[])
+{
+	int settingDefaultValue = GET_JSON_SETTING_INT(systelab::setting::SettingsService(), TestSettingsFile, TestSetting)
+	std::cout << "Setting default value: " << settingDefaultValue << std::endl;
+
+    return 0;
+}
