46736f46178c98ef93d84b5864a24af2ca946f12
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-10-31 20:30:43 +0100

    Fixed errors on definition macros

--- src/JSONSettings/Model/SettingDefinitionMacros.h
+++ src/JSONSettings/Model/SettingDefinitionMacros.h
@@ -14,76 +14,53 @@
 #define JSON_SETTINGS_FILE(SETTINGS_FILE_NAMESPACE, SETTINGS_FILENAME, CONTENT) \
 	namespace SETTINGS_FILE_NAMESPACE \
 	{ \
-		static const std::string FILENAME = SETTINGS_FILENAME; \
+		static const std::string FILENAME = #SETTINGS_FILENAME; \
 		CONTENT \
 	}
 
-#define JSON_SETTING_INT(SETTING_NAME, SETTING_PATH, SETTING_DEFAULT, USE_CACHE) \
+#define JSON_SETTING_INT(SETTING_NAME, SETTING_PATH, SETTING_DEFAULT_VALUE, USE_CACHE) \
 	namespace SETTING_NAME \
 	{ \
-		static const std::string SETTING_PATH = #SETTING_PATH; \
-		static const int DEFAULT_VALUE = SETTING_DEFAULT; \
+		static const std::string PATH = #SETTING_PATH; \
+		static const int DEFAULT_VALUE = SETTING_DEFAULT_VALUE; \
 		struct SETTING_NAME##SettingStruct \
 		{ \
 			SETTING_NAME##SettingStruct() \
 			{ \
-				systelab::setting::SettingDefinition settingDefinition = { DEFAULT_VALUE, NO_VALIDATION_FUNCTION, USE_CACHE }; \
-				systelab::setting::SettingDefinitionMgr::get().setSetting(FILENAME, SETTING_PATH, settingDefinition); \
+				systelab::setting::SettingDefinition settingDefinition = { DEFAULT_VALUE, NO_VALIDATION_FUNCTION(), USE_CACHE }; \
+				systelab::setting::SettingDefinitionMgr::get().setSetting(FILENAME, PATH, settingDefinition); \
 			} \
 		}; \
 		static SETTING_NAME##SettingStruct structInstance; \
 	}
 
-
-#define JSON_SETTING_INT_BOUNDED(SETTING_NAME, SETTING_PATH, SETTING_DEFAULT, MIN_BOUND, MAX_BOUND, USE_CACHE) \
+#define JSON_SETTING_STR(SETTING_NAME, SETTING_PATH, SETTING_DEFAULT_VALUE, USE_CACHE) \
 	namespace SETTING_NAME \
 	{ \
-		static const std::string SETTING_PATH = #SETTING_PATH; \
-		static const int DEFAULT_VALUE = SETTING_DEFAULT; \
+		static const std::string PATH = #SETTING_PATH; \
+		static const std::string DEFAULT_VALUE = #SETTING_DEFAULT_VALUE; \
 		struct SETTING_NAME##SettingStruct \
 		{ \
 			SETTING_NAME##SettingStruct() \
 			{ \
-				systelab::setting::SettingDefinition::ValidationFunction boundCheck = [](const SettingDefinition::SettingValue& v) -> bool \
-																	{ \
-																		bool aboveMinBound = (boost::get<int>(v) >= MIN_BOUND); \
-																		bool belowMaxBound = (boost::get<int>(v) <= MAX_BOUND); \
-																		return (aboveMinBound && belowMaxBound); \
-																	}; \
-				systelab::setting::SettingDefinition settingDefinition = { DEFAULT_VALUE, boundCheck, USE_CACHE }; \
-				systelab::setting::SettingDefinitionMgr::get().setSetting(FILENAME, SETTING_PATH, settingDefinition); \
+				systelab::setting::SettingDefinition settingDefinition = { DEFAULT_VALUE, NO_VALIDATION_FUNCTION(), USE_CACHE }; \
+				systelab::setting::SettingDefinitionMgr::get().setSetting(FILENAME, PATH, settingDefinition); \
 			} \
 		}; \
 		static SETTING_NAME##SettingStruct structInstance; \
 	}
 
-#define JSON_SETTING_STRING(SETTING_NAME, SETTING_PATH, SETTING_DEFAULT, USE_CACHE) \
+#define JSON_SETTING_BOOL(SETTING_NAME, SETTING_PATH, SETTING_DEFAULT_VALUE, USE_CACHE) \
 	namespace SETTING_NAME \
 	{ \
-		static const std::string SETTING_PATH = #SETTING_PATH; \
-		static const std::string DEFAULT_VALUE = SETTING_DEFAULT; \
+		static const std::string PATH = #SETTING_PATH; \
+		static const bool DEFAULT_VALUE = SETTING_DEFAULT_VALUE; \
 		struct SETTING_NAME##SettingStruct \
 		{ \
 			SETTING_NAME##SettingStruct() \
 			{ \
-				systelab::setting::SettingDefinition settingDefinition = { DEFAULT_VALUE, NO_VALIDATION_FUNCTION, USE_CACHE }; \
-				systelab::setting::SettingDefinitionMgr::get().setSetting(FILENAME, SETTING_PATH, settingDefinition); \
-			} \
-		}; \
-		static SETTING_NAME##SettingStruct structInstance; \
-	}
-
-#define JSON_SETTING_BOOL(SETTING_NAME, SETTING_PATH, SETTING_DEFAULT, USE_CACHE) \
-	namespace SETTING_NAME \
-	{ \
-		static const std::string SETTING_PATH = #SETTING_PATH; \
-		static const bool DEFAULT_VALUE = SETTING_DEFAULT; \
-		struct SETTING_NAME##SettingStruct \
-		{ \
-			SETTING_NAME##SettingStruct() \
-			{ \
-				systelab::setting::SettingDefinition settingDefinition = { DEFAULT_VALUE, NO_VALIDATION_FUNCTION, USE_CACHE }; \
-				systelab::setting::SettingDefinitionMgr::get().setSetting(FILENAME, SETTING_PATH, settingDefinition); \
+				systelab::setting::SettingDefinition settingDefinition = { DEFAULT_VALUE, NO_VALIDATION_FUNCTION(), USE_CACHE }; \
+				systelab::setting::SettingDefinitionMgr::get().setSetting(FILENAME, PATH, settingDefinition); \
 			} \
 		}; \
 		static SETTING_NAME##SettingStruct structInstance; \
