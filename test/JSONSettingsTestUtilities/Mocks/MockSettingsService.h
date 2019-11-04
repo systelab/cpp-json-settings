6ff033983961358223c2593ca40dd84639c198ce
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-11-04 23:13:58 +0100

    Fixed signatures to satisfy defined interface

--- test/JSONSettingsTestUtilities/Mocks/MockSettingsService.h
+++ test/JSONSettingsTestUtilities/Mocks/MockSettingsService.h
@@ -12,12 +12,12 @@ namespace systelab { namespace setting { namespace test_utility {
 		virtual ~MockSettingsService();
 
 		MOCK_CONST_METHOD2(getSettingInteger, int(const std::string&, const std::string&));
-		MOCK_CONST_METHOD2(getSettingBoolean, int(const std::string&, const std::string&));
+		MOCK_CONST_METHOD2(getSettingBoolean, bool(const std::string&, const std::string&));
 		MOCK_CONST_METHOD2(getSettingString, std::string(const std::string&, const std::string&));
 
-		MOCK_METHOD3(setSettingInteger, int(const std::string&, const std::string&, int));
-		MOCK_METHOD3(setSettingBoolean, int(const std::string&, const std::string&, bool));
-		MOCK_METHOD3(setSettingString, int(const std::string&, const std::string&, const std::string&));
+		MOCK_METHOD3(setSettingInteger, void(const std::string&, const std::string&, int));
+		MOCK_METHOD3(setSettingBoolean, void(const std::string&, const std::string&, bool));
+		MOCK_METHOD3(setSettingString, void(const std::string&, const std::string&, const std::string&));
 
 		MOCK_METHOD0(clearCache, void());
 	};
