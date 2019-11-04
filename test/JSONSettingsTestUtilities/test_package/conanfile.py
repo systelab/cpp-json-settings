eb271ff295bfc1decf7c943f001964ec367250af
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-11-04 23:33:06 +0100

    Fixed test package for JSONSettingsTestUtilities

--- test/JSONSettingsTestUtilities/test_package/conanfile.py
+++ test/JSONSettingsTestUtilities/test_package/conanfile.py
@@ -2,15 +2,15 @@ import os
 from conans import ConanFile, CMake, tools
 
 
-class JWTUtilsTestUtilitiesTestConan(ConanFile):
+class JSONSettingsTestUtilitiesTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
-    options = {"gtest": ["1.7.0", "1.8.1"], "OpenSSL": ["1.0.2n"]}
-    default_options = {"gtest":"1.8.1", "OpenSSL":"1.0.2n"}
+    options = {"boost": ["1.66.0", "1.67.0"], "gtest": ["1.7.0", "1.8.1"]}
+    default_options = {"boost":"1.67.0", "gtest":"1.8.1"}
 
     def configure(self):
-        self.options["JWTUtilsTestUtilities"].gtest = self.options.gtest
-        self.options["JWTUtilsTestUtilities"].OpenSSL = self.options.OpenSSL
+        self.options["JSONSettingsTestUtilities"].boost = self.options.boost
+        self.options["JSONSettingsTestUtilities"].gtest = self.options.gtest
 
     def build(self):
         cmake = CMake(self)
