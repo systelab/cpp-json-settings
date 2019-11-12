7bd2b007254e01244643f120c325daee540f4623
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2019-11-13 00:33:31 +0100

    Added tests for section settings

---
+++ test/JSONSettingsTest/Tests/SettingsServiceSectionTest.cpp
@@ -0,0 +1,198 @@
+#include "stdafx.h"
+#include "SettingsServiceBaseTest.h"
+
+
+namespace systelab { namespace setting { namespace unit_test {
+
+	class SettingsServiceSectionTest : public SettingsServiceBaseTest
+	{
+	public:
+		void SetUp()
+		{
+			SettingsServiceBaseTest::SetUp();
+		}
+
+		void TearDown()
+		{
+			SettingsServiceBaseTest::TearDown();
+			SettingsService().clearCache();
+		}
+
+		void writeSectionSettingsIntoFile(int intValue, const std::string& strValue, bool boolValue)
+		{
+			std::stringstream ss;
+			ss << "{" << std::endl;
+			ss << "    \"Section\": " << std::endl;
+			ss << "    {" << std::endl;
+			ss << "        \"IntSettingCache\": \"" << intValue << "\"," << std::endl;
+			ss << "        \"StrSettingCache\": \"" << strValue << "\"," << std::endl;
+			ss << "        \"BoolSettingCache\": \"" << (boolValue ? "true" : "false") << "\"" << std::endl;
+			ss << "    }" << std::endl;
+			ss << "}";
+
+			std::string fileContents = ss.str();
+			writeSettingsFile(fileContents);
+		}
+	};
+
+
+	// GetSettings
+	TEST_F(SettingsServiceSectionTest, testGetSettingsReturnsDefaultValuesWhenNoFileNorCache)
+	{
+		EXPECT_EQ(4321, GET_JSON_SETTING_INT (SettingsService(), MySettingsFile, SectionIntSetting));
+		EXPECT_EQ("ba", GET_JSON_SETTING_STR (SettingsService(), MySettingsFile, SectionStrSetting));
+		EXPECT_EQ(true, GET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testGetSettingsReturnsValuesFromFileWhenNotInCache)
+	{
+		writeSectionSettingsIntoFile(123456, "file", false);
+
+		EXPECT_EQ(123456, GET_JSON_SETTING_INT (SettingsService(), MySettingsFile, SectionIntSetting));
+		EXPECT_EQ("file", GET_JSON_SETTING_STR (SettingsService(), MySettingsFile, SectionStrSetting));
+		EXPECT_EQ(false,  GET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testGetSettingsReturnsValuesFromCacheWhenAlreadyRead)
+	{
+		GET_JSON_SETTING_INT (SettingsService(), MySettingsFile, SectionIntSetting);
+		GET_JSON_SETTING_STR (SettingsService(), MySettingsFile, SectionStrSetting);
+		GET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting);
+
+		writeSectionSettingsIntoFile(1111, "AAAA", false);
+
+		EXPECT_EQ(4321, GET_JSON_SETTING_INT (SettingsService(), MySettingsFile, SectionIntSetting));
+		EXPECT_EQ("ba", GET_JSON_SETTING_STR (SettingsService(), MySettingsFile, SectionStrSetting));
+		EXPECT_EQ(true, GET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting));
+	}
+
+
+	// SetSettings - Not existing file
+	TEST_F(SettingsServiceSectionTest, testSetSettingIntWritesValueIntoFile)
+	{
+		SET_JSON_SETTING_INT(SettingsService(), MySettingsFile, SectionIntSetting, 7777);
+		std::string expectedJSON = "{ \"Section\": { \"IntSettingCache\": \"7777\" } }";
+		EXPECT_TRUE(json::test_utility::compareJSONs(expectedJSON, readSettingsFile(), m_jsonAdapter));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testSetSettingStrWritesValueIntoFile)
+	{
+		SET_JSON_SETTING_STR(SettingsService(), MySettingsFile, SectionStrSetting, "NewValue");
+		std::string expectedJSON = "{ \"Section\": { \"StrSettingCache\": \"NewValue\" } }";
+		EXPECT_TRUE(json::test_utility::compareJSONs(expectedJSON, readSettingsFile(), m_jsonAdapter));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testSetSettingBoolWritesValueIntoFile)
+	{
+		SET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting, true);
+		std::string expectedJSON = "{ \"Section\": { \"BoolSettingCache\": \"true\" } }";
+		EXPECT_TRUE(json::test_utility::compareJSONs(expectedJSON, readSettingsFile(), m_jsonAdapter));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testSetSettingsWritesAllValuesIntoFile)
+	{
+		SET_JSON_SETTING_INT(SettingsService(), MySettingsFile, SectionIntSetting, 7777);
+		SET_JSON_SETTING_STR(SettingsService(), MySettingsFile, SectionStrSetting, "NewValue");
+		SET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting, true);
+
+		std::stringstream ss;
+		ss << "{" << std::endl;
+		ss << "    \"Section\": " << std::endl;
+		ss << "    {" << std::endl;
+		ss << "        \"IntSettingCache\": \"7777\"," << std::endl;
+		ss << "        \"StrSettingCache\": \"NewValue\"," << std::endl;
+		ss << "        \"BoolSettingCache\": \"true\"" << std::endl;
+		ss << "    }" << std::endl;
+		ss << "}" << std::endl;
+
+		std::string expectedJSON = ss.str();
+		EXPECT_TRUE(json::test_utility::compareJSONs(expectedJSON, readSettingsFile(), m_jsonAdapter));
+	}
+
+
+	// SetSettings - Existing file
+	TEST_F(SettingsServiceSectionTest, testSetSettingIntUpdatesValueFromExistingFile)
+	{
+		writeSectionSettingsIntoFile(1111, "AAAA", false);
+
+		SET_JSON_SETTING_INT(SettingsService(), MySettingsFile, SectionIntSetting, 2222);
+
+		std::stringstream ss;
+		ss << "{" << std::endl;
+		ss << "    \"Section\": " << std::endl;
+		ss << "    {" << std::endl;
+		ss << "        \"IntSettingCache\": \"2222\"," << std::endl;
+		ss << "        \"StrSettingCache\": \"AAAA\"," << std::endl;
+		ss << "        \"BoolSettingCache\": \"false\"" << std::endl;
+		ss << "    }" << std::endl;
+		ss << "}" << std::endl;
+
+		std::string expectedJSON = ss.str();
+		EXPECT_TRUE(json::test_utility::compareJSONs(expectedJSON, readSettingsFile(), m_jsonAdapter));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testSetSettingStrUpdatesValueFromExistingFile)
+	{
+		writeSectionSettingsIntoFile(1111, "AAAA", false);
+
+		SET_JSON_SETTING_STR(SettingsService(), MySettingsFile, SectionStrSetting, "BBBBBB");
+
+		std::stringstream ss;
+		ss << "{" << std::endl;
+		ss << "    \"Section\": " << std::endl;
+		ss << "    {" << std::endl;
+		ss << "        \"IntSettingCache\": \"1111\"," << std::endl;
+		ss << "        \"StrSettingCache\": \"BBBBBB\"," << std::endl;
+		ss << "        \"BoolSettingCache\": \"false\"" << std::endl;
+		ss << "    }" << std::endl;
+		ss << "}" << std::endl;
+
+		std::string expectedJSON = ss.str();
+		EXPECT_TRUE(json::test_utility::compareJSONs(expectedJSON, readSettingsFile(), m_jsonAdapter));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testSetSettingBoolUpdatesValueFromExistingFile)
+	{
+		writeSectionSettingsIntoFile(1111, "AAAA", false);
+
+		SET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting, true);
+
+		std::stringstream ss;
+		ss << "{" << std::endl;
+		ss << "    \"Section\": " << std::endl;
+		ss << "    {" << std::endl;
+		ss << "        \"IntSettingCache\": \"1111\"," << std::endl;
+		ss << "        \"StrSettingCache\": \"AAAA\"," << std::endl;
+		ss << "        \"BoolSettingCache\": \"true\"" << std::endl;
+		ss << "    }" << std::endl;
+		ss << "}" << std::endl;
+
+		std::string expectedJSON = ss.str();
+		EXPECT_TRUE(json::test_utility::compareJSONs(expectedJSON, readSettingsFile(), m_jsonAdapter));
+	}
+
+
+	// SetSettings - Cache updated after set
+	TEST_F(SettingsServiceSectionTest, testSetSettingIntUpdatesCache)
+	{
+		SET_JSON_SETTING_INT(SettingsService(), MySettingsFile, SectionIntSetting, 7777);
+		boost::filesystem::remove(m_settingsFilepath);
+		EXPECT_EQ(7777, GET_JSON_SETTING_INT(SettingsService(), MySettingsFile, SectionIntSetting));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testSetSettingStrUpdatesCache)
+	{
+		SET_JSON_SETTING_STR(SettingsService(), MySettingsFile, SectionStrSetting, "NewValue");
+		boost::filesystem::remove(m_settingsFilepath);
+		EXPECT_EQ("NewValue", GET_JSON_SETTING_STR(SettingsService(), MySettingsFile, SectionStrSetting));
+	}
+
+	TEST_F(SettingsServiceSectionTest, testSetSettingBoolUpdatesCache)
+	{
+		SET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting, true);
+		boost::filesystem::remove(m_settingsFilepath);
+		EXPECT_EQ(true, GET_JSON_SETTING_BOOL(SettingsService(), MySettingsFile, SectionBoolSetting));
+	}
+
+}}}
+
