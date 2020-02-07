7551400e93eb43fbeb89df8b0d00d878e2c20735
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-02-07 01:56:44 +0100

    Moved to boost 1.71.0

--- test/JSONSettingsTestUtilities/conanfile.py
+++ test/JSONSettingsTestUtilities/conanfile.py
@@ -11,8 +11,8 @@ class JSONSettingsTestUtilitiesConan(ConanFile):
     license = "MIT"
     generators = "cmake_find_package"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"boost": ["1.66.0", "1.67.0"], "gtest": ["1.7.0", "1.8.1"]}
-    default_options = {"boost":"1.67.0", "gtest":"1.8.1"}
+    options = {"boost": ["1.66.0", "1.67.0", "1.71.0"], "gtest": ["1.7.0", "1.8.1","1.10.0"]}
+    default_options = {"boost":"1.71.0", "gtest":"1.10.0"}
     exports_sources = "*"
 
     def configure(self):
@@ -22,8 +22,10 @@ class JSONSettingsTestUtilitiesConan(ConanFile):
     def requirements(self):
         if self.options.gtest == "1.7.0":
             self.requires("gtest/1.7.0@systelab/stable")
-        else:
+        elif self.options.gtest == "1.8.1":
             self.requires("gtest/1.8.1@bincrafters/stable")
+        else:
+            self.requires("gtest/1.10.0@systelab/stable")
 
         if ("%s" % self.version) == "None":
             self.requires("JSONSettings/%s@systelab/stable" % os.environ['VERSION'])
