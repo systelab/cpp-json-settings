53697bec2571090904a371b30fdc6eaeed5f4f6f
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-07 18:04:56 +0200

    Fixed version of EncryptionAdapter of gtest

--- conanfile.py
+++ conanfile.py
@@ -18,6 +18,7 @@ class JSONSettingsConan(ConanFile):
         self.options["boost"].shared = True
         self.options["RapidJSONAdapter"].gtest = self.options.gtest
         self.options["JSONAdapterTestUtilities"].gtest = self.options.gtest
+        self.options["EncryptionAdapterTestUtilities"].gtest = self.options.gtest
         self.options["CaeserCypherEncryptionAdapter"].gtest = self.options.gtest
 
     def requirements(self):
