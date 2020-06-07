d44eb4b1b15b80cef75af075575766d20dc7a850
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-07 17:39:01 +0200

    Moved to latest version of encryption libraries

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,7 @@ class JSONSettingsConan(ConanFile):
         self.options["CaeserCypherEncryptionAdapter"].gtest = self.options.gtest
 
     def requirements(self):
-        self.requires("EncryptionAdapterInterface/1.0.2@systelab/stable")
+        self.requires("EncryptionAdapterInterface/1.0.3@systelab/stable")
         if self.options.boost == "1.66.0":
             self.requires("boost/1.66.0@conan/stable")
         elif self.options.boost == "1.67.0":
@@ -30,9 +30,9 @@ class JSONSettingsConan(ConanFile):
             self.requires(("boost/%s") % self.options.boost)
 
     def build_requirements(self):
-        self.build_requires("CaeserCypherEncryptionAdapter/1.0.1@systelab/stable")
+        self.build_requires("CaeserCypherEncryptionAdapter/1.0.2@systelab/stable")
         self.build_requires("RapidJSONAdapter/1.0.8@systelab/stable")
-        self.build_requires("EncryptionAdapterTestUtilities/1.0.2@systelab/stable")
+        self.build_requires("EncryptionAdapterTestUtilities/1.0.3@systelab/stable")
         self.build_requires("JSONAdapterTestUtilities/1.0.11@systelab/stable")
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
