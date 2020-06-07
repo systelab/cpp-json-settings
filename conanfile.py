9d7aec96b2777d3a310bdaa07f5bda94f48ac8c5
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-07 17:00:19 +0200

    Added test utilities for encryption adapter as a dependency of tests

--- conanfile.py
+++ conanfile.py
@@ -32,6 +32,7 @@ class JSONSettingsConan(ConanFile):
     def build_requirements(self):
         self.build_requires("CaeserCypherEncryptionAdapter/1.0.1@systelab/stable")
         self.build_requires("RapidJSONAdapter/1.0.8@systelab/stable")
+        self.build_requires("EncryptionAdapterTestUtilities/1.0.2@systelab/stable")
         self.build_requires("JSONAdapterTestUtilities/1.0.11@systelab/stable")
         if self.options.gtest == "1.7.0":
             self.build_requires("gtest/1.7.0@systelab/stable")
