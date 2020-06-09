5381eaaf3ed8720acdbda678e3643548b3d9aac2
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-09 21:13:21 +0200

    Added includes to be shared within the whole library

---
+++ src/JSONSettingsRESTAPI/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING 1
+
+// STL
+#include <functional>
+#include <istream>
+#include <ostream>
+#include <map>
+#include <string>
+#include <vector>
