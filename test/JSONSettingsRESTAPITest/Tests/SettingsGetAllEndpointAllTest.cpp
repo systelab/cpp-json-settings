71939c1081f0cc616d113a90dce1397ac8de5ce7
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-10 20:32:41 +0200

    Fixed compilation of unit test

--- test/JSONSettingsRESTAPITest/Tests/SettingsGetAllEndpointAllTest.cpp
+++ test/JSONSettingsRESTAPITest/Tests/SettingsGetAllEndpointAllTest.cpp
@@ -3,6 +3,11 @@
 
 #include "TestSettingsDefinition.h"
 
+#include "JSONSettings/SettingsService.h"
+#include "RapidJSONAdapter/JSONAdapter.h"
+#include "RESTAPICore/Endpoint/EndpointRequestData.h"
+#include "WebServerAdapterInterface/Model/Reply.h"
+
 
 using namespace testing;
 
@@ -13,18 +18,18 @@ namespace systelab { namespace setting { namespace rest_api { namespace unit_tes
 	public:
 		void SetUp()
 		{
-			auto settingsService = std::make_unique<SettingsService>();
+			auto settingsService = std::make_unique<systelab::setting::SettingsService>();
 			m_endpoint = std::make_unique<SettingsGetAllEndpoint>("MySettingsFile.json", std::move(settingsService), m_jsonAdapter);
 		}
 
-	private:
+	protected:
 		std::unique_ptr<systelab::rest_api_core::IEndpoint> m_endpoint;
 		systelab::json::rapidjson::JSONAdapter m_jsonAdapter;
 	};
 
 	TEST_F(SettingsGetAllEndpointTest, testGetSettingsReturnsValuesFromFile)
 	{
-		// TODO
+		ASSERT_ANY_THROW(m_endpoint->execute(rest_api_core::EndpointRequestData()));
 	}
 
 }}}}
