db4656fe24499eace8aba7347de80f61c005fa11
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-10 19:42:14 +0200

    Added missing includes

--- src/JSONSettingsRESTAPI/stdafx.h
+++ src/JSONSettingsRESTAPI/stdafx.h
@@ -3,11 +3,17 @@
 #define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING 1
 
 // STL
+#include <fstream>
 #include <functional>
 #include <iomanip>
+#include <iostream>
 #include <istream>
+#include <limits>
+#include <map>
+#include <memory>
 #include <ostream>
 #include <map>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
