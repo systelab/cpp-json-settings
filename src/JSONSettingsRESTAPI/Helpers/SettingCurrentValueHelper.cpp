d3850d2e7007714bbf6a8de2a041bec40cd34104
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-11 00:57:44 +0200

    Added helper to easily access setting current value

---
+++ src/JSONSettingsRESTAPI/Helpers/SettingCurrentValueHelper.cpp
@@ -0,0 +1,35 @@
+﻿#include "stdafx.h"
+#include "SettingCurrentValueHelper.h"
+
+#include "JSONSettings/ISettingsService.h"
+
+
+namespace systelab { namespace setting { namespace rest_api {
+
+	SettingValue SettingCurrentValueHelper::query(const ISettingsService& service,
+												  const SettingsFile& file,
+												  const SettingPath& path,
+												  const SettingValueType& valueType)
+	{
+		SettingValue currentValue;
+		if (valueType == SettingValueType::IntValue)
+		{
+			currentValue = SettingValue(service.getSettingInteger(file, path)).value;
+		}
+		else if (valueType == SettingValueType::DoubleValue)
+		{
+			currentValue = SettingValue(service.getSettingDouble(file, path)).value;
+		}
+		else if (valueType == SettingValueType::BooleanValue)
+		{
+			currentValue = SettingValue(service.getSettingBoolean(file, path)).value;
+		}
+		else if (valueType == SettingValueType::StringValue)
+		{
+			currentValue = SettingValue(service.getSettingString(file, path)).value;
+		}
+
+		return currentValue;
+	}
+
+}}}
---
+++ src/JSONSettingsRESTAPI/Helpers/SettingCurrentValueHelper.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "JSONSettings/SettingsFile.h"
+#include "JSONSettings/SettingPath.h"
+#include "JSONSettings/SettingValue.h"
+
+
+namespace systelab { namespace setting {
+	class ISettingsService;
+}}
+
+namespace systelab { namespace setting { namespace rest_api {
+
+	class SettingCurrentValueHelper
+	{
+	public:
+		static SettingValue query(const ISettingsService&, const SettingsFile&, const SettingPath&, const SettingValueType&);
+	};
+
+}}}
