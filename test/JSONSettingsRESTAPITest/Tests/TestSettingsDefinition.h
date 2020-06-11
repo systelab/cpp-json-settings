4cb07668617ca7fed676871d2f2f4705ff9de06d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-11 10:50:06 +0200

    Removed unnecessary second test file

--- test/JSONSettingsRESTAPITest/Tests/TestSettingsDefinition.h
+++ test/JSONSettingsRESTAPITest/Tests/TestSettingsDefinition.h
@@ -15,13 +15,6 @@ namespace systelab { namespace setting { namespace rest_api { namespace unit_tes
 		JSON_SETTING_BOOL(SubsectionBoolSetting, "Section.Subsection.BoolSettingNoCache", false, CACHE_DISABLED)
 	);
 
-	JSON_SETTINGS_FILE(SecondSettingsFile, "SecondSettingsFile.json",
-		JSON_SETTING_INT (RootIntSettingCache,   "IntSettingCache",     2222, CACHE_ENABLED)
-		JSON_SETTING_DBL (RootDblSettingCache,   "DblSettingCache",    2.222, CACHE_ENABLED)
-		JSON_SETTING_STR (RootStrSettingCache,   "StrSettingCache", "SECOND", CACHE_ENABLED)
-		JSON_SETTING_BOOL(RootBoolSettingCache, "BoolSettingCache",    false, CACHE_ENABLED)
-	);
-	
 	JSON_SETTINGS_ENCRYPTED_FILE(EncryptedSettingsFile, "EncryptedSettingsFile.json", []() { return "Key"; },
 		JSON_SETTING_INT(SectionIntSetting,    "Section.IntSettingCache", 9999, CACHE_ENABLED)
 		JSON_SETTING_DBL(SectionDblSetting,    "Section.DblSettingCache",  9.9, CACHE_ENABLED)
