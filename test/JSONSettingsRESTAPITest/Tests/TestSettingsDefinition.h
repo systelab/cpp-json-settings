f95bde63c35e0c5bd0c82b702014e78ea1a9a6e6
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-11 10:27:45 +0200

    Improved definition of my settings file

--- test/JSONSettingsRESTAPITest/Tests/TestSettingsDefinition.h
+++ test/JSONSettingsRESTAPITest/Tests/TestSettingsDefinition.h
@@ -9,17 +9,16 @@
 namespace systelab { namespace setting { namespace rest_api { namespace unit_test {
 
 	JSON_SETTINGS_FILE(MySettingsFile, "MySettingsFile.json",
-
-		JSON_SETTING_INT (RootIntSettingCache,  "IntSettingCache",  1234, CACHE_ENABLED)
-		JSON_SETTING_DBL (RootDblSettingNoCache,  "DblSettingNoCache",  5.678, CACHE_DISABLED)
-		JSON_SETTING_STR (SectionStrSetting,	 "Section.StrSettingCache",  "ba", CACHE_ENABLED)
-		JSON_SETTING_BOOL(SubsectionBoolSetting, "Section.Subsection.BoolSettingCache", false, CACHE_DISABLED)
+		JSON_SETTING_INT (RootIntSettingCache,                         "IntSettingCache",  1234, CACHE_ENABLED)
+		JSON_SETTING_DBL (RootDblSettingNoCache,                     "DblSettingNoCache", 5.678, CACHE_DISABLED)
+		JSON_SETTING_STR (SectionStrSetting,                   "Section.StrSettingCache",  "ba", CACHE_ENABLED)
+		JSON_SETTING_BOOL(SubsectionBoolSetting, "Section.Subsection.BoolSettingNoCache", false, CACHE_DISABLED)
 	);
 
 	JSON_SETTINGS_FILE(SecondSettingsFile, "SecondSettingsFile.json",
-		JSON_SETTING_INT (RootIntSettingCache,  "IntSettingCache",      2222, CACHE_ENABLED)
-		JSON_SETTING_DBL (RootDblSettingCache,  "DblSettingCache",     2.222, CACHE_ENABLED)
-		JSON_SETTING_STR (RootStrSettingCache,  "StrSettingCache",  "SECOND", CACHE_ENABLED)
+		JSON_SETTING_INT (RootIntSettingCache,   "IntSettingCache",     2222, CACHE_ENABLED)
+		JSON_SETTING_DBL (RootDblSettingCache,   "DblSettingCache",    2.222, CACHE_ENABLED)
+		JSON_SETTING_STR (RootStrSettingCache,   "StrSettingCache", "SECOND", CACHE_ENABLED)
 		JSON_SETTING_BOOL(RootBoolSettingCache, "BoolSettingCache",    false, CACHE_ENABLED)
 	);
 	
