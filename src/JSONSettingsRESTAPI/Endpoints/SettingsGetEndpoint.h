45ec0fcebdfc5be9146e440514b453a834489b6e
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-11 09:08:00 +0200

    Added endpoints for settings GET

---
+++ src/JSONSettingsRESTAPI/Endpoints/SettingsGetEndpoint.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "RESTAPICore/Endpoint/IEndpoint.h"
+
+#include "JSONSettings/SettingsFile.h"
+#include "JSONSettings/SettingPath.h"
+#include <boost/optional.hpp>
+
+
+namespace systelab {
+
+	namespace json {
+		class IJSONAdapter;
+	}
+	namespace setting {
+		class ISettingsService;
+	}
+}
+
+namespace systelab { namespace setting { namespace rest_api {
+
+	class SettingsGetEndpoint final : public systelab::rest_api_core::IEndpoint
+	{
+	public:
+		SettingsGetEndpoint(const SettingsFile& settingsFile,
+							std::unique_ptr<ISettingsService>,
+							const systelab::json::IJSONAdapter&);
+		virtual ~SettingsGetEndpoint();
+
+		std::unique_ptr<systelab::web_server::Reply> execute(const systelab::rest_api_core::EndpointRequestData&) override;
+
+	protected:
+		boost::optional<SettingPath> findSetting(int settingId) const;
+
+		virtual std::unique_ptr<systelab::web_server::Reply> buildSettingNotFoundReply() const;
+		virtual std::unique_ptr<systelab::web_server::Reply> buildInternalErrorReply(const std::string& errorMessage) const;
+
+	protected:
+		const SettingsFile& m_settingsFile;
+		std::unique_ptr<ISettingsService> m_settingsService;
+		const systelab::json::IJSONAdapter& m_jsonAdapter;
+	};
+
+}}}
+
