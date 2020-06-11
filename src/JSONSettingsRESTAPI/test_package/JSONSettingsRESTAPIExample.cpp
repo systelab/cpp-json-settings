35144d9daec6467fcb13f5ebecfa06f46e89023d
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-11 13:46:48 +0200

    Execute endpoint on test package application

--- src/JSONSettingsRESTAPI/test_package/JSONSettingsRESTAPIExample.cpp
+++ src/JSONSettingsRESTAPI/test_package/JSONSettingsRESTAPIExample.cpp
@@ -14,15 +14,18 @@
 #include "JSONSettings/SettingsService.h"
 
 #include "RapidJSONAdapter/JSONAdapter.h"
+#include "RESTAPICore/Endpoint/EndpointRequestData.h"
 
 
 int main(int argc, char *argv[])
 {
-	systelab::setting::SettingsFile settingsFile = "MySettingsFile";
+	systelab::setting::SettingsFile settingsFile = "MySettingsFile.json";
 	auto settingsService = std::make_unique<systelab::setting::SettingsService>();
 	systelab::json::rapidjson::JSONAdapter jsonAdapter;
 	
 	auto endpoint = std::make_unique<systelab::setting::rest_api::SettingsGetAllEndpoint>
 						(settingsFile, std::move(settingsService), jsonAdapter);
+	endpoint->execute(systelab::rest_api_core::EndpointRequestData());
+
     return 0;
 }
