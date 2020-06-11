24bf6ab6beb8930284080eeda8d4b6f5372fbf61
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-06-11 09:33:16 +0200

    Reduced the number of settings in test settings files

--- test/JSONSettingsRESTAPITest/Tests/TestSettingsDefinition.h
+++ test/JSONSettingsRESTAPITest/Tests/TestSettingsDefinition.h
@@ -10,27 +10,9 @@ namespace systelab { namespace setting { namespace unit_test {
 
 	JSON_SETTINGS_FILE(MySettingsFile, "MySettingsFile.json",
 
-		// Root settings
 		JSON_SETTING_INT (RootIntSettingCache,  "IntSettingCache",  1234, CACHE_ENABLED)
-		JSON_SETTING_DBL (RootDblSettingCache,  "DblSettingCache", 1.234, CACHE_ENABLED)
-		JSON_SETTING_STR (RootStrSettingCache,  "StrSettingCache",  "ab", CACHE_ENABLED)
-		JSON_SETTING_BOOL(RootBoolSettingCache, "BoolSettingCache", true, CACHE_ENABLED)
-
-		JSON_SETTING_INT (RootIntSettingNoCache,  "IntSettingNoCache",   5678, CACHE_DISABLED)
 		JSON_SETTING_DBL (RootDblSettingNoCache,  "DblSettingNoCache",  5.678, CACHE_DISABLED)
-		JSON_SETTING_STR (RootStrSettingNoCache,  "StrSettingNoCache",   "cd", CACHE_DISABLED)
-		JSON_SETTING_BOOL(RootBoolSettingNoCache, "BoolSettingNoCache", false, CACHE_DISABLED)
-
-		// Section settings
-		JSON_SETTING_INT (SectionIntSetting,  "Section.IntSettingCache",  4321, CACHE_ENABLED)
-		JSON_SETTING_DBL (SectionDblSetting,  "Section.DblSettingCache", 4.321, CACHE_ENABLED)
-		JSON_SETTING_STR (SectionStrSetting,  "Section.StrSettingCache",  "ba", CACHE_ENABLED)
-		JSON_SETTING_BOOL(SectionBoolSetting, "Section.BoolSettingCache", true, CACHE_ENABLED)
-
-		// Subsection settings
-		JSON_SETTING_INT (SubsectionIntSetting,  "Section.Subsection.IntSettingCache",   8765, CACHE_DISABLED)
-		JSON_SETTING_DBL (SubsectionDblSetting,  "Section.Subsection.DblSettingCache",  8.765, CACHE_DISABLED)
-		JSON_SETTING_STR (SubsectionStrSetting,  "Section.Subsection.StrSettingCache",   "dc", CACHE_DISABLED)
+		JSON_SETTING_STR (SectionStrSetting,	 "Section.StrSettingCache",  "ba", CACHE_ENABLED)
 		JSON_SETTING_BOOL(SubsectionBoolSetting, "Section.Subsection.BoolSettingCache", false, CACHE_DISABLED)
 	);
 
