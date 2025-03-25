eb4d5fe76a3daacefe6a183f3219e8d892f22146
jlpeiro <jpeiro@werfen.com>
2025-03-25 17:23:30 +0100

    Cleaned test package conanfile of JSONSettingsRESTAPI

--- src/JSONSettingsRESTAPI/test_package/conanfile.py
+++ src/JSONSettingsRESTAPI/test_package/conanfile.py
@@ -4,15 +4,7 @@ from conans import ConanFile, CMake
 class JSONSettingsRESTAPITestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
-    options = {"boost": ["1.66.0", "1.67.0", "1.72.0"], "openssl": ["1.0.2n", "1.0.2s", "1.1.1g"], "gtest": ["1.7.0", "1.8.1", "1.10.0"]}
-    default_options = {"boost":"1.72.0", "openssl": "1.1.1g", "gtest":"1.10.0"}
     required_conan_version = ">=1.33.1"
-
-    def configure(self):
-        self.options["JSONSettingsRESTAPI"].boost = self.options.boost
-        self.options["JSONSettingsRESTAPI"].openssl = self.options.openssl
-        self.options["JSONSettingsRESTAPI"].gtest = self.options.gtest
-        self.options["RapidJSONAdapter"].gtest = self.options.gtest
 
     def requirements(self):
         self.requires("RapidJSONAdapter/1.1.6@systelab/stable")
